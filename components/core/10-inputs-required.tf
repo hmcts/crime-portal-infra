5c9500c78f7c3ecc09509d98ab640fd8f130a619
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-08 09:05:03 +0100

    fix: formatting

--- components/core/10-inputs-required.tf
+++ components/core/10-inputs-required.tf
@@ -4,16 +4,16 @@ variable "location" {
 }
 
 variable "env" {
-  type = string
+  type        = string
   description = "The environment to deploy resources to."
 }
 
 variable "builtFrom" {
-  type = string
+  type        = string
   description = "The name of the repository these resources are builtFrom."
 }
 
 variable "product" {
-  type = string
+  type        = string
   description = "The name of the prodcut this infrastructure supports."
 }
