02822ac0ab8232b050798f0603bd6d76e40aa1f7
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-08 08:47:52 +0100

    feat: initial commit for crime-portal-infra

---
+++ components/core/10-inputs-required.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  type        = string
+  description = "The Azure region to deploy resources to."
+}
+
+variable "env" {
+  type = string
+  description = "The environment to deploy resources to."
+}
+
+variable "builtFrom" {
+  type = string
+  description = "The name of the repository these resources are builtFrom."
+}
+
+variable "product" {
+  type = string
+  description = "The name of the prodcut this infrastructure supports."
+}
