bee50d9ee8b625291b4e3a886b73524794bf6860
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-08 12:55:49 +0100

    feat: added basic postgres impl

---
+++ components/backend/12-interpolated-defaults.tf
@@ -0,0 +1,30 @@
+locals {
+  env_map = {
+    "stg"  = "nle"
+    "prod" = "prod"
+  }
+  is_prod     = length(regexall(".*(prod).*", var.env)) > 0
+  admin_group = local.is_prod ? "DTS Platform Operations SC" : "DTS Platform Operations"
+}
+
+module "ctags" {
+  source = "github.com/hmcts/terraform-module-common-tags"
+
+  builtFrom   = var.builtFrom
+  environment = var.env
+  product     = var.product
+}
+
+data "azurerm_subnet" "backend" {
+  name                 = "crime-portal-backend-${var.env}"
+  virtual_network_name = "vnet-${local.env_map[var.env]}-int-01"
+  resource_group_name  = "InternalSpoke-rg"
+}
+
+data "azuread_group" "admin_group" {
+  display_name     = local.admin_group
+  security_enabled = true
+}
+
+data "azurerm_client_config" "current" {}
+
---
+++ components/backend/20-postgresql.tf
@@ -0,0 +1,26 @@
+module "postgresql" {
+
+  providers = {
+    azurerm.postgres_network = azurerm
+  }
+
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  env    = var.env
+
+  product       = var.product
+  component     = "backend"
+  business_area = "dlrm"
+
+  pgsql_databases = [
+    {
+      name : "application"
+    }
+  ]
+
+  pgsql_version = "14"
+
+  pgsql_delegated_subnet_id = data.azurerm_subnet.backend.id
+  admin_user_object_id      = data.azurerm_client_config.current.object_id
+
+  common_tags = var.common_tags
+}
