a956a61c8e1446e6b0e653fe8f6e76faf9affde9
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-11 09:32:37 +0100

    fix: specify install extensions

--- components/backend/25-ldap.tf
+++ components/backend/25-ldap.tf
@@ -53,4 +53,9 @@ module "virtual_machine" {
   vm_version           = "latest"
   vm_availabilty_zones = each.value.availability_zone
   tags                 = module.ctags.common_tags
+
+  install_azure_monitor      = true
+  install_dynatrace_oneagent = true
+  install_splunk_uf          = true
+  nessus_install             = true
 }
