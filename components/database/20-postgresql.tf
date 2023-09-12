585b1522892708c23836ed85b36f7a0196dc8a9a
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-12 10:52:48 +0100

    fix: disable stg postgres deployment

--- components/database/20-postgresql.tf
+++ components/database/20-postgresql.tf
@@ -11,10 +11,11 @@ module "postgresql" {
   business_area       = "dlrm"
   resource_group_name = local.resource_group_name
 
-  pgsql_databases           = var.postgres_databases
-  pgsql_version             = var.pgsql_version
-  pgsql_delegated_subnet_id = data.azurerm_subnet.backend-postgresql.id
-  admin_user_object_id      = data.azurerm_client_config.current.object_id
+  pgsql_databases               = var.postgres_databases
+  pgsql_version                 = var.pgsql_version
+  pgsql_delegated_subnet_id     = data.azurerm_subnet.backend-postgresql.id
+  admin_user_object_id          = data.azurerm_client_config.current.object_id
+  enable_read_only_group_access = false
 
   common_tags = module.ctags.common_tags
 }
