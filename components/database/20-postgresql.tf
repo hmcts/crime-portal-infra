05b40cb72d967472625b9d0a81fac7ba23811b6b
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-12 13:32:07 +0100

    fix: switch back to master branch and update admin oid

--- components/database/20-postgresql.tf
+++ components/database/20-postgresql.tf
@@ -3,7 +3,7 @@ module "postgresql" {
     azurerm.postgres_network = azurerm
   }
 
-  source = "git::https://github.com/hmcts/terraform-module-postgresql-flexible?ref=fix%2Fset-db-permissions-script-ado-fix"
+  source = "git::https://github.com/hmcts/terraform-module-postgresql-flexible?ref=master"
 
   env                 = var.env
   product             = var.product
@@ -14,7 +14,7 @@ module "postgresql" {
   pgsql_databases           = var.postgres_databases
   pgsql_version             = var.pgsql_version
   pgsql_delegated_subnet_id = data.azurerm_subnet.backend-postgresql.id
-  admin_user_object_id      = data.azurerm_client_config.current.object_id
+  admin_user_object_id      = "ca6d5085-485a-417d-8480-c3cefa29df31"
 
   common_tags = module.ctags.common_tags
 }
