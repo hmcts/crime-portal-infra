c48a2b996e2eadb955083235ee738d4e3bed45f2
nicholas chang <nicholasc001@hotmail.com>
2024-01-25 11:27:39 +0000

    remove unwanted data block

--- components/app-gateway/12-interpolated-defaults.tf
+++ components/app-gateway/12-interpolated-defaults.tf
@@ -77,11 +77,6 @@ data "azurerm_key_vault_secret" "root_certificates" {
   key_vault_id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.KeyVault/vaults/crime-portal-kv-${var.env}"
 }
 
-# data "azurerm_key_vault_secret" "ssl_certificates" {
-#   for_each     = { for ssl_cert in local.ssl_certificates : ssl_cert.ssl_cert_key => ssl_cert }
-#   name         = each.value.ssl_cert_name
-#   key_vault_id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.KeyVault/vaults/crime-portal-kv-${var.env}"
-# }
 
 data "azurerm_key_vault" "acme_kv" {
   provider            = azurerm.acme
