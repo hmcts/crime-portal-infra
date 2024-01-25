55a164d8beb8fda707a9636db7d4c222b3e0d69c
nicholas chang <nicholasc001@hotmail.com>
2024-01-25 15:26:22 +0000

    fix-keyvault-id-typo

--- components/app-gateway/12-interpolated-defaults.tf
+++ components/app-gateway/12-interpolated-defaults.tf
@@ -81,12 +81,10 @@ data "azurerm_key_vault_secret" "root_certificates" {
 data "azurerm_key_vault" "acme_kv" {
   provider            = azurerm.acme
   name                = var.app_gateway.ssl_certificates["certificate"].key_vault_name
-  resource_group_name = "cft-platform-ptl-rg"
+  resource_group_name = local.acme_resource_group
 }
 
 data "azurerm_key_vault_certificate" "certificate" {
   name         = var.app_gateway.ssl_certificates["certificate"].certificate_name
   key_vault_id = data.azurerm_key_vault.acme_kv.id
 }
-
-
