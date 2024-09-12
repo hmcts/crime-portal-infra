3370f913cd0922c6c789eebb4f0e0774516f3145
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-09-12 12:02:14 +0100

    Rolling back the install-clamav-update-splunk branch to master

--- components/vm/25-vms.tf
+++ components/vm/25-vms.tf
@@ -6,7 +6,7 @@ module "virtual-machines" {
   }
 
   for_each                = local.virtual_machines
-  source                  = "github.com/hmcts/terraform-module-virtual-machine.git?ref=feat%2Finstall-clamav-update-splunk"
+  source                  = "github.com/hmcts/terraform-module-virtual-machine.git?ref=master"
   vm_type                 = "linux"
   vm_name                 = each.key
   env                     = var.env == "stg" ? "nonprod" : var.env
