5dacba619a98f263d0b3b7ad9568e02177c7870f
nicholas chang <nicholasc001@hotmail.com>
2024-10-14 16:46:51 +0100

    edit postgresql branch

--- components/core/00-init.tf
+++ components/core/00-init.tf
@@ -4,8 +4,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.5.0"
-     }
+      version = "3.97.1"
+    }
   }
 
   backend "azurerm" {}
